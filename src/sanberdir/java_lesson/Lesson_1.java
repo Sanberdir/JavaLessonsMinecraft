6720df4c21e36107e41bcc38b6e2d5113be2466d
Sanberdir <91392232+Sanberdir@users.noreply.github.com>
2025-11-15 13:08:52 +0300

    Initial commit

---
+++ src/sanberdir/java_lesson/Lesson_1.java
@@ -0,0 +1,12 @@
+package sanberdir.java_lesson;
+
+public class Lesson_1 {
+
+    public static void main(String[] arguments) {
+        System.out.println("Привет ученики!");
+        System.out.println("Как дела?");
+        System.out.printf("Всё хорошо");
+        System.out.println("Пока!");
+
+    }
+}
