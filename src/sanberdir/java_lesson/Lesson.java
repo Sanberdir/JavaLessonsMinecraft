4753c0b2fc19f82bf96efa1cff321a71ad219122
Sanberdir <91392232+Sanberdir@users.noreply.github.com>
2025-11-16 14:54:55 +0300

    3

---
+++ src/sanberdir/java_lesson/Lesson.java
@@ -0,0 +1,21 @@
+package sanberdir.java_lesson;
+
+public class Lesson {
+
+    public static void main(String[] arguments) {
+        /* БАЗОВЫЙ СИНТАКСИС JAVA */
+
+        /* Переменные */
+
+        // тип_данных имя_переменной; // Объявление (Declaration)
+        // ИЛИ
+        // тип_данных имя_переменной = значение_переменной; // Инициализация (Declaration + Assignment of Value)
+
+        int points;     // Объявление
+        points = 100;   // Присваивание значения
+        int x = -69;    // Инициализация (Объявление + Присваивание)
+
+        // Каждая инструкция/строка заканчивается точкой с запятой ;
+        int y = 42;
+    }
+}
