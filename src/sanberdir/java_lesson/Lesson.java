51cb32231cbb5e4eabe07dc34522ee9fbc0f29ea
Sanberdir <91392232+Sanberdir@users.noreply.github.com>
2026-02-01 15:38:11 +0300

    23 anonimus classes

--- src/sanberdir/java_lesson/Lesson.java
+++ src/sanberdir/java_lesson/Lesson.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Lesson {
 
     public static void main(String[] args) {
-        /* INTERFACES & ABSTRACT CLASSES */
+
 
         Animal bengie = new Dog("bengie.png", "Bengie", 9);
         Animal jeremy = new Dog("jeremy.png", "Jeremy", 7);
@@ -15,6 +15,19 @@ public class Lesson {
         Animal george = new Cat("george.jpg", "George", 3);
 
         Bird jules = new Bird("jules.png", "Jules", 1);
+        /*Анонимные классы*/
+        Animal joel = new Animal("joel.png","joel",1) {
+
+            @Override
+            public void move() {
+                System.out.println("Хомячок бежит");
+            }
+
+            @Override
+            public void makeSound() {
+                System.out.println("Хомячок пищит");
+            }
+        };
 
         jules.move();
         jules.fly();
@@ -23,12 +36,13 @@ public class Lesson {
         animals.add(bengie);
         animals.add(whiskers);
         animals.add(jules);
+        animals.add(joel);
 
         List<IFlyable> flyables = new ArrayList<>();
         flyables.add(jules);
         flyables.add(new Aeroplane());
 
-        for(Animal animal : animals) {
+        for (Animal animal : animals) {
             animal.move();
             animal.makeSound();
         }
