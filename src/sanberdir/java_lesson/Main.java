abed98d0c94732741cf5f83029130f696a288094
Санбердир <aleksander.zhuckov2011@gmail.com  aleksander.zhuckov2011@gmail.com>
2026-03-17 14:22:02 +0300

    /* Lambda & Streams */

--- src/sanberdir/java_lesson/Main.java
+++ src/sanberdir/java_lesson/Main.java
@@ -1,16 +1,41 @@
 package sanberdir.java_lesson;
 
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int x = 10;
-        try {
-            float a = 100 / 0;
-        } catch (Exception e) {
-            System.out.println("Это счастливое исключение " + e.getMessage());
-        }
-        System.out.println("I am happy!");
-        if (x < 100) {
-            throw new Exception("Это просто изумительно!");
-        }
+    public static void main(String[] args) {
+        /* Lambda & Streams */
+        helloWorld();
+
+        Runnable runnable = () -> System.out.println("Hello World");
+        somethingCool(runnable);
+        somethingCool(Main::helloWorld);
+
+        Supplier<Float> pi = () -> 3.14159265f;
+
+        Consumer<Runnable> test = runnable1 -> {
+            runnable1.run();
+
+        };
+        test.accept(runnable);
+
+        Runnable method = Main::helloWorld;
+
+        List<String> fruits = List.of("Apple", "Banana", "Orange", "Mango", "Pineapple", "Mandarin");
+
+        fruits.stream()
+                .filter(fruit -> fruit.startsWith("M"))
+                .forEach(System.out::println);
+    }
+    public static void helloWorld(){
+        System.out.println("Bye World");
+    }
+    public static void somethingCool(Runnable runnable){
+        runnable.run();
+    }
+    public static float getPi(){
+        return 3.14159265f;
     }
 }
